914f82b3234faf83c6b5765befadd06bdd78ff7e
Miša Skalskis <Skalskis.M@gmail.com>
2014-06-13 12:17:49 +0200

    PeerAssessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a list of functions. By using set function, it creates
+## a function for you, so you don't need to specify matrix before-hand.
+## I've found that solve works only with small square matrices.
+## So, be careful, i guess.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric(), nrow, ncol) {
+    m <- NULL
+    set <- function(y, nrow, ncol) {
+        x <<- matrix(y, nrow, ncol)
+        m <<- NULL
+    }
+    get <- function() x
+    setInv <- function(Inv) m <<- Inv
+    getInv <- function() m
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Gets an inverse of the matrix. If the inverse was already calculated, i take
+## it from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInv(m)
+    m
 }
